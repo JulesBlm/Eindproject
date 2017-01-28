89cca0293f50a3eafcc20ab1bda02c79fa60179c
Jules <julesblom@gmail.com>
2017-01-28 13:06:43 +0100

    Beta versie met Barchart animations

--- kladcode.js
+++ kladcode.js
@@ -31,47 +31,113 @@ function drawTreemap(passedTrade) {
 
 
   /* ------ TREEMAP ANIMATION ------*/ 
-  // function changeYear(selectedYear) {
-  //     var t = d3.transition()
-  //       .duration(750);
+function transitionTreemap/(selectedYear) {
+.
+    treemapData = data[country][selectedYear]
 
-  //     treemapData = data[country][selectedYear]
+    console.log("Changed treemapData:", treemapData)
 
-  //     console.log("Changed treemapData:", treemapData)
+    var root = d3.hierarchy(treemapData)
+      .sum(function(d) { return d.import; });
+    
+    treemap(root);
+    console.log("new tm'ed root", root)
 
-  //     var root = d3.hierarchy(treemapData)
-  //       .sum(function(d) { return d.import; });
-      
-  //     treemap(root);
-  //     console.log("new tm'ed root", root)
+    // Join new data with old elements, if any.
+    square.data(root.leaves())
+    
+    // EXIT old elements not present in new data.
+    square.exit()
+      .transition(t)
+      .style("fill-opacity", 1e-6)
+      .remove();
 
-  //     // Join new data with old elements, if any.
-  //     square.data(root.leaves())
-      
-  //     // EXIT old elements not present in new data.
-  //     square.exit()
-  //         .attr("class", "exit")
-  //       .transition(t)
-  //         .attr("y", 60)
-  //         .style("fill-opacity", 1e-6)
-  //         .remove();
+    // The new enter set
+    square.enter().append("text")
+    square.enter()
+      .transition(t)
+        .attr("transform", function(d) { return "translate(" + d.x0 + "," + d.y0 + ")"; })
+      .select("rect")
+        .attr("width", function(d) { return d.x1 - d.x0; })
+        .attr("height", function(d) { return d.y1 - d.y0; })
 
-  //     // UPDATE old elements present in new data.
-  //     square.enter()
-  //       .attr("class", "update")
-  //       .transition(t)
-  //         .attr("transform", function(d) { return "translate(" + d.x0 + "," + d.y0 + ")"; })
-  //       .select("rect")
-  //         .attr("width", function(d) { return d.x1 - d.x0; })
-  //         .attr("height", function(d) { return d.y1 - d.y0; })
+    // the "UPDATE" set:
+    square.enter()
+        .attr("id", function(d) { return d.data.country; })
+      .transition(t)
+        .attr("width", function(d) { return d.x1 - d.x0; })
+        .attr("height", function(d) { return d.y1 - d.y0; })
+        .attr("fill", function(d) { return colorTreemap(d.data.import); });
+  
+  // Remove exit selection
+  square.exit()
+    .transition(t)
+    .style('fill-opacity', 1e-6)
+    .remove();
 
-  //     // ENTER new elements present in new data.
-  //     square.enter().append("text")
-  //         .attr("class", "enter")
-  //         .attr("id", function(d) { return d.data.country; })
-  //       .transition(t)
-  //         .attr("width", function(d) { return d.x1 - d.x0; })
-  //         .attr("height", function(d) { return d.y1 - d.y0; })
-  //         .attr("fill", function(d) { return colorTreemap(d.data.import); });
-  //   }
+  square.enter().append("rect")
+    .attr("class", "treemap-country")
+    .attr("id", function(d) { return d.data.country; })
+    .attr("width", function(d) {
+      // console.log(d.data.country, "x1:", d.x1, "x0:", d.x0);
+      if ( Number.isNaN(d.x1) || Number.isNaN(d.x0) ) {}
+      else { return (d.x1 - d.x0) };
+    })
+    .attr("height", function(d) { 
+      // console.log(d.data.country,"y1:", d.y1, "y0:", d.y0); 
+      if ( Number.isNaN(d.y1) || Number.isNaN(d.y0) ) {}
+      else { return (d.y1 - d.y0) };
+    })
+    .attr("fill", function(d) { return colorTreemap(d.data[passedTrade]); })
+    .on("mousemove", function(d) { showTooltip(d.data.country, (d.data[passedTrade]/1000000), "million mystery units", d3.select("#treemap")) ; })
+    .on("mouseout", function() { tooltip.classed("hidden", true); })
+    .on("click", function() { updateBarchart(selectedCountry, this.id, year, tradeString); } ); 
 
+
+  square.transition()
+      .duration(300)
+    .attr("class", "treemap-country")
+    .attr("id", function(d) { return d.data.country; })
+    .attr("width", function(d) {
+      // console.log(d.data.country, "x1:", d.x1, "x0:", d.x0);
+      if ( Number.isNaN(d.x1) || Number.isNaN(d.x0) ) {}
+      else { return (d.x1 - d.x0) };
+    })
+    .attr("height", function(d) { 
+      // console.log(d.data.country,"y1:", d.y1, "y0:", d.y0); 
+      if ( Number.isNaN(d.y1) || Number.isNaN(d.y0) ) {}
+      else { return (d.y1 - d.y0) };
+    })
+    .attr("fill", function(d) { return colorTreemap(d.data[passedTrade]); })
+    .on("mousemove", function(d) { showTooltip(d.data.country, (d.data[passedTrade]/1000000), "million mystery units", d3.select("#treemap")) ; })
+    .on("mouseout", function() { tooltip.classed("hidden", true); })
+    .on("click", function() { updateBarchart(selectedCountry, this.id, year, tradeString); } ); 
+
+  }
+//--------------------------
+    // Remove old
+    bars.exit()
+      .transition()
+        .duration(300)
+      // .attr("y", function(d) { return yBarchart(d.data); })
+      .style('fill-opacity', 1e-6)
+      .remove();
+
+    // The new enter set
+    bars.enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return xBarchart(d.year); })
+      .attr("y", function(d) { return yBarchart(d.data); })
+      .attr("width", xBarchart.bandwidth() )
+      .attr("height", function(d) { return bcHeight - yBarchart(d.data); })
+      .style("fill", function(d) { return colorTreemap(d.data) });
+
+    // the "UPDATE" set:
+    bars.transition()
+      .duration(300)
+        .attr("class", "bar")
+        .attr("x", function(d) { return xBarchart(d.year); })
+        .attr("y", function(d) { return yBarchart(d.data); })
+        .attr("width", xBarchart.bandwidth() )
+        .attr("height", function(d) { return bcHeight - yBarchart(d.data); })
+        .style("fill", function(d) { return colorTreemap(d.data) });
